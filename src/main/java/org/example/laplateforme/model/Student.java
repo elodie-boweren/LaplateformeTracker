d3be91b390ee28ca05ab4acdb8ba027faf750408
Laura-2115 <laura.iriskinian@laplateforme.io>
2025-07-02 14:34:22 +0200

    add student managment

--- src/main/java/org/example/laplateforme/model/Student.java
+++ src/main/java/org/example/laplateforme/model/Student.java
@@ -1,4 +1,79 @@
 package org.example.laplateforme.model;
 
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String grade;
+
+    //Constructeurs
+    public Student(){}
+
+    public Student(int id, String firstName, String lastName, int age, String grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public Student(String firstName, String lastName, int age, String grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    //toString pour affichage
+    @Override
+    public String toString() {
+        return "Student {" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
 }
